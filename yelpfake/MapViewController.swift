95627269e9d8152fad993230cb9be7b586cc32b5
Sung Kang <sungjk@msn.com>
2015-11-20 11:59:04 -0800

    working on map view 0.1

--- yelpfake/MapViewController.swift
+++ yelpfake/MapViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
 
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager: CLLocationManager!
+    var restaurants: [Restaurant!]!
+    
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+            print("location manager")
+        }
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations.last! as CLLocation
+//        myLocation = Artwork(title: "Annotation1", locationName: "locationName", discipline: "generic Location", coordinate: CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude))
+//        mapView.addAnnotation(myLocation)
+        print(location)
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        print(center)
+        self.mapView.setRegion(region, animated: true)
+    }
+    
+    // sets the rectangular region to display to get correct zoom level
+//    let regionRadius: CLLocationDistance = 1000
+//    func centerMapOnLocation(location: CLLocation) {
+//        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+//            regionRadius * 2.0, regionRadius * 2.0)
+//        mapView.setRegion(coordinateRegion, animated: true)
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
