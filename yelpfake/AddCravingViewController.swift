f7b7894e566685547694a5ff01813e80273c25ba
Paul Kwon <paul.s.kwon92@gmail.com>
2015-11-20 13:51:34 -0800

    fixed the craving+ button

--- yelpfake/AddCravingViewController.swift
+++ yelpfake/AddCravingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  AddCravingViewController.swift
+//  addCravingsViewController.swift
 //  yelpfake
 //
 //  Created by Paul Kwon on 11/20/15.
@@ -9,29 +9,19 @@
 import UIKit
 
 class addCravingViewController: UITableViewController {
+    
     weak var cancelButtonDelegate: CancelButtonDelegate?
+    
     
     @IBAction func cancelButton(sender: UIBarButtonItem) {
         cancelButtonDelegate?.CancelButtonPressed(self)
     }
     
     @IBAction func doneButton(sender: UIBarButtonItem) {
-        
-       // if let craving = cravingToEdit {
-        //    craving.rest_name = rest
-       // }
     }
     
     @IBAction func locationButton(sender: UIButton) {
-        //self.tag =
     }
+    @IBOutlet weak var nameText: UITextField!
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "LocationSWay" {
-            let navigationController = segue.destinationViewController as! UINavigationController
-            let controller = navigationController.topViewController as! MapViewController
-            
-        }
-    }
-
 }
